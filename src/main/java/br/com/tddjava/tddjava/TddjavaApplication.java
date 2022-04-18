b9ae7fd06c81272d51e5b4e5e4aa1228e5babc03
Daniele <danileaodev@gmail.com>
2022-04-18 16:07:13 -0300

    Create

---
+++ src/main/java/br/com/tddjava/tddjava/TddjavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.tddjava.tddjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tddjava/tddjava/TddjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tddjava.tddjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
